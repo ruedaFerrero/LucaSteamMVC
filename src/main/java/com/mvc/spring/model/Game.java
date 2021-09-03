3ec9d8ab816c472f7f4b938de21689e2a203017c
Julio <julio@ruedaferrero.com>
2021-09-03 10:18:24 +0200

    Fixed index.html Game setter

--- src/main/java/com/mvc/spring/model/Game.java
+++ src/main/java/com/mvc/spring/model/Game.java
@@ -45,6 +45,14 @@ public class Game implements Serializable {
         this.euSales = euSales;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
